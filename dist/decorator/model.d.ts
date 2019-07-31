abbd746132354651ec886fa240c793c22e1d39a8
Marsch Huynh <marsch.huynh@gmail.com>
2019-07-31 18:19:46 +0700

    refactor: item_url now is optinal

--- dist/decorator/model.d.ts
+++ dist/decorator/model.d.ts
@@ -3,6 +3,6 @@ declare type AttributeType = {
     required?: boolean;
     pattern?: string;
 };
-export declare function Model(list_url: string, item_url: string, namespace?: string): (target: any) => any;
+export declare function Model(list_url: string, item_url?: string, namespace?: string): (target: any) => any;
 export declare function Attribute(option?: AttributeType): Function;
 export {};
