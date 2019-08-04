60d9b78b3e0eb3b406e27ae8a16d4e9b7df463b7
Marsch Huynh <marsch.huynh@gmail.com>
2019-08-04 18:06:29 +0700

    chore: minor update

--- dist/module.d.ts
+++ dist/module.d.ts
@@ -2,6 +2,8 @@ export interface ModuleOption {
     BASE_URL: string;
     LIST_DATA_KEY: string;
     LIST_META_KEY: string;
+    item_transform: () => any;
+    list_transform: () => any;
 }
 export default class Module {
     private static INSTANCE;
